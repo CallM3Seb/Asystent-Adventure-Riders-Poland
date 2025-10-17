fecf1c01a9015f27cb64a35ffa01d639a6c34dc1
CallM3Seb <sdrzewiecki@gmail.com>
2025-10-17 11:21:38 +0200

    Update prompt text for useful links

--- lib/config.ts
+++ lib/config.ts
@@ -8,7 +8,7 @@ export const CREATE_SESSION_ENDPOINT = "/api/create-session";
 export const STARTER_PROMPTS: StartScreenPrompt[] = [
   {
     label: "Pokaż przydatne linki",
-    prompt: "Pokaż przydatne linki",
+    prompt: "pokaż listę przydatnych linków",
     icon: "circle-question",
   },
 ];
